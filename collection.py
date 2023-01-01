8ca80f8c5e21791bb1ca3ae0cde4813ca27a0d7d
warhir <krzyzopolski.mariusz@gmail.com>
2023-01-01 19:48:28 +0100

    API shelves boilerplate

--- collection.py
+++ collection.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, FastAPI, HTTPException
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class Item(BaseModel):
+    name: str
+    type: str
+    description: str
+    image: str
+
+class Shelf(BaseModel):
+    name: str
+    clothing_items: List[Item]
+
+shelves = {}
+database = {}
 
 @router.post("/sets/{set_id}")
 async def post_set(set_id):
@@ -12,12 +27,22 @@ async def post_set(set_id):
 async def get_set(set_id):
     return {}
 
-
+def get_empty_shelve():
+    return next(x for x in shelves if len(x) == 0)
 @router.post("/items/{item_id}")
-async def post_item(item_id):
-    return {}
+async def post_item(item: Item):
+    database[item.name] = item
+    shelves[get_empty_shelve().name] = item
+    return {"success": True}
 
 
 @router.get("/items/{item_id}")
-async def get_item(item_id):
-    return {}
+async def get_item(item_name):
+    return {"item": database[item_name]}
+
+@router.put("/shelves/{shelf_id}")
+async def put_item_on_shelf(shelf_id: int, clothing_items: List[Item]):
+    if shelf_id not in shelves:
+        raise HTTPException(status_code=404, detail="Shelf not found")
+    shelves[shelf_id].clothing_items = clothing_items
+    return {"success": True}
--- user.py
+++ user.py
@@ -1,16 +1,31 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 router = APIRouter()
+class User(BaseModel):
+    username: str
+    password: str
 
-
+database = {}
 @router.post("/login")
-async def user_login():
-    return {}
+async def user_login(user: User):
+    if database[user.username].password == user.password:
+        return {"success": True}
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect password")
+    # else:
+    #     raise HTTPException(status_code=404, detail="User not found")
 
 
 @router.post("/register")
-async def user_register():
-    return {}
+async def user_register(user: User):
+    if user.username in database:
+        raise HTTPException(status_code=400, detail="Username already taken")
+    database[user.username] = user
+    return {"success": True}
 
 
 @router.post("/logout")
