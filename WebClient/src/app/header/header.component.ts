1dd65d7f5309c94b36ebabb062da29dc223df139
Joanna Walkiewicz <s20161@pjwstk.edu.pl>
2023-03-30 19:55:30 +0200

    add logout

--- WebClient/src/app/header/header.component.ts
+++ WebClient/src/app/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../authService/auth-service.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
+
 })
 export class HeaderComponent {
-  private router: Router
-  
-  logout() {
-    this.router.navigate([ '/app-login' ])
+
+  constructor(
+    public authService: AuthService,
+    private router: Router) {
   }
-  
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login'])
+  }
 }
