b403dc69e8a689713f44ee1266f191ce68e403a7
Joanna Walkiewicz <s20161@pjwstk.edu.pl>
2023-03-31 18:11:21 +0200

    .

--- WebClient/src/app/auth-interceptor/auth-interceptor.ts
+++ WebClient/src/app/auth-interceptor/auth-interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Component, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 
 
