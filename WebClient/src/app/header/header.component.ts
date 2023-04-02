81408a23bed34b78770a1d9ee9e653d78f85fa33
s20161-pj <56191700+s20161-pj@users.noreply.github.com>
2023-04-02 20:58:41 +0200

    Add login and register page (#9)
    * Add login and register page
    
    * Modified login and register components
    
    * Add login and register page
    
    * Modified login and register components
    
    * login body
    
    * Update login page
    
    * add logout
    
    * python style
    
    * .
    
    * complete login and register page
    
    * add log out and changes in slider
    
    ---------
    
    Co-authored-by: mariuszkrzyzopolski <krzyzopolski.mariusz@gmail.com>
    Co-authored-by: mariuszkrzyzopolski <39222705+mariuszkrzyzopolski@users.noreply.github.com>

--- WebClient/src/app/header/header.component.ts
+++ WebClient/src/app/header/header.component.ts
@@ -1,10 +1,21 @@
-import { Component} from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../authService/auth-service.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
+
 })
 export class HeaderComponent {
 
+  constructor(
+    public authService: AuthService,
+    private router: Router) {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login'])
+  }
 }
