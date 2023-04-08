6584496e0a20f0ffb48fbdedf8feca1d9b1fe5f6
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2023-04-08 12:06:06 +0200

    First test

---
+++ Tests/test_user.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+import requests
+import json
+
+from API.database import DB, get_database
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class TestUser(TestCase):
+    def setUp(self):
+        conn = get_database()
+        database = DB(conn)
+        Base.metadata.drop_all(conn)
+        database.initialize_db()
+
+    def test_user_register(self):
+        self.setUp()
+        url = "http://localhost:8000/user/register"
+        user_data = {
+            "mail": "test@test.com",
+            "city": "Gdansk",
+            "password": "password",
+            "repeated_password": "password"
+        }
+
+        response = requests.post(url, data=json.dumps(user_data), headers={"Content-Type": "application/json"})
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_second_user(self):
+        self.setUp()
+        url = "http://localhost:8000/user/register"
+        user_data = {
+            "mail": "test@test.com",
+            "city": "Gdansk",
+            "password": "password",
+            "repeated_password": "password"
+        }
+        requests.post(url, data=json.dumps(user_data), headers={"Content-Type": "application/json"})
+        response = requests.post(url, data=json.dumps(user_data), headers={"Content-Type": "application/json"})
+        self.assertNotEqual(response.status_code, 200)
